b28ac40c2324e8bb6061c469f5205f62e5521ae6
Caleb Roland <60106973+Neighborkid01@users.noreply.github.com>
2021-12-09 14:54:47 -0500

    Ok, just trying it as a worker

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const client = redis.createClient({
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
-    socketMode: false,
+    socketMode: true,
     appToken: process.env.SLACK_APP_TOKEN
 });
 
